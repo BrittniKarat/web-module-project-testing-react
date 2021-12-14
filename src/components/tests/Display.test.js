89e7a13d5147b2e1a304f0542fce7ee6d04ac62a
BunniMK <86545296+BunniMK@users.noreply.github.com>
2021-12-13 17:45:12 -0700

    Added tests

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+import fetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
 
-test('renders without errors with no props', ()=>{
+const data = {
+    name: "",
+    summary: "",
+    seasons: [
+        { id: 1, name: "caterpillar", episodes: [] },
+        { id: 2, name: "cocoon", episodes: [] },
+        { id: 3, name: "butterfly", episodes: [] }
+    ]
+}
 
+test('renders without errors with no props', () => {
+    render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async () => {
+    fetchShow.mockResolvedValueOnce(data);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    render(<Display show={data} />);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const showComponent = await screen.findByTestId("show-container");
+
+    expect(showComponent).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', async () => {
+    fetchShow.mockResolvedValueOnce(data);
+
+    render(<Display />);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    
+    await waitFor(() => {
+        const seasons = screen.getAllByTestId("season-option");
+        expect(seasons).toHaveLength(3);
+    });
+});
+
+test('displayFunc is called when the fetch button is pressed', async () => {
+    fetchShow.mockResolvedValueOnce(data);
+
+    const displayFunc = jest.fn();
+
+    render(<Display displayFunc={displayFunc} />);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled();
+    });
+});
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,30 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const data = {
+    id: 909347,
+    image: "https://static.tvmaze.com/uploads/images/medium_landscape/132/332055.jpg",
+    name: "Chapter Seven: The Lost Sister",
+    season: 2,
+    number: 7,
+    summary: "El travels to Chicago to find her sister Eight, aka Kali, who has teamed up with other outcasts to kill the men who tormented her at Hawkins Labs.",
+    runtime: 46
+}
 
-test("renders without error", () => {});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders without error", () => {
+    render(<Episode episode={data} />);
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={data}/>);
+
+    const sum = screen.queryByText(/El travels to Chicago to find her sister Eight, aka Kali, who has teamed up with other outcasts to kill the men who tormented her at Hawkins Labs/i);
+
+    expect(sum).toBeTruthy();
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={data} />);
+
+});
